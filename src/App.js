bfaeee4edaa3e560a8a74bd99db4fdd3fa0c1018
Diloes <diloes@hotmail.com>
2022-01-01 23:01:57 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="login"> 
+      <h2 className="login__title">OpenBootcamp <span>| Alumnos</span></h2>
+
+      <form className="login__form">
+        <label for="correo"><b>Email</b></label>
+        <input type="email" placeholder="Introduce tu correo" id="correo" />
+
+        <label for="contraseña"><b>Contraseña</b></label>
+        <input type="password" placeholder="Introduce tu contraseña" id="contraseña"></input>
+
+        <div class="login__checkbox">
+						<div>
+						<input type="checkbox" id="recuerdame"/>
+						<label for="recuerdame">Recuerdame</label>
+						</div>
+						<p>He olvidado la contraseña</p>
+				</div>
+      
+        <button class="login__btn">Iniciar Sesión</button>
+      </form>
     </div>
   );
 }
