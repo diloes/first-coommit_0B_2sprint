6122dff7347b1dfd7a89e7ad01be90e37f4c3e63
Diloes <diloes@hotmail.com>
2022-01-02 12:02:18 +0100

    add footer

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,26 +4,33 @@ const Login = () => {
     return (
         <div>
             <div className="login"> 
-            <h2 className="login__title">OpenBootcamp <span>| Alumnos</span></h2>
+                <h2 className="login__title">OpenBootcamp <span>| Alumnos</span></h2>
 
-            <form className="login__form">
-                <label for="correo"><b>Email</b></label>
-                <input type="email" placeholder="Introduce tu correo" id="correo" />
+                <form className="login__form">
+                    <label for="correo"><b>Email</b></label>
+                    <input type="email" placeholder="Introduce tu correo" id="correo" />
 
-                <label for="contraseña"><b>Contraseña</b></label>
-                <input type="password" placeholder="Introduce tu contraseña" id="contraseña"></input>
+                    <label for="contraseña"><b>Contraseña</b></label>
+                    <input type="password" placeholder="Introduce tu contraseña" id="contraseña"></input>
 
-                <div class="login__checkbox">
-                                <div>
-                                <input type="checkbox" id="recuerdame"/>
-                                <label for="recuerdame">Recuerdame</label>
-                                </div>
-                                <p>He olvidado la contraseña</p>
+                    <div class="login__checkbox">
+                        <div>
+                            <input type="checkbox" id="recuerdame"/>
+                            <label for="recuerdame">Recuerdame</label>
                         </div>
-            
-                <button class="login__btn">Iniciar Sesión</button>
-                <p>Click en iniciar sesión para ver tabla ^</p>
-            </form>
+                        <p>He olvidado la contraseña</p>
+                    </div>
+                
+                    <button class="login__btn">Iniciar Sesión</button>
+                    <p>Click aquí para ver la tabla</p>
+                </form>
+
+                <div class="footer">
+					<p>Copyright © 2021 Open Bootcamp SL, Imagina Group</p>
+					<p>Todos los derechos reservados.</p>
+					<p><a href="#">Política de Privacidad</a></p>
+				</div>
+                
             </div>
         </div>
     )
